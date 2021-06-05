8b146dd30d646645d5277f6f67e383d89aa0c709
sun.xf <sunxuefeng.sxf@mapgis.com>
2021-06-05 12:31:53 +0800

    影院界面+引入滚动条betterScroll

--- src/views/Cinema.vue
+++ src/views/Cinema.vue
@@ -1,11 +1,56 @@
 <template>
-  <div>
-    cineama
+  <div class="cinema" :style="{ height: height }">
+    <ul>
+      <li v-for="cinema in cinemaList" :key="cinema.cinemaId">
+        <div>{{ cinema.name }}</div>
+        <div class="address">{{ cinema.address }}</div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+import http from '@/util/http'
+import BetterScroll from 'better-scroll'
+export default {
+  data () {
+    return {
+      cinemaList: [],
+      height: '0'
+    }
+  },
+  mounted () {
+    this.height = document.documentElement.clientHeight - 50 + 'px'
+    http({
+      url: '/gateway?cityId=110100&ticketFlag=1&k=7115329',
+      headers: {
+        'X-Host': 'mall.film-ticket.cinema.list'
+      }
+    }).then(res => {
+      this.cinemaList = res.data.data.cinemas
+      this.$nextTick(() => {
+        new BetterScroll('.cinema', {
+          scrollbar: {
+            fade: true
+          }
+        })
+      })
+    })
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.cinema {
+  height: 300px;
+  overflow: hidden;
+  position: relative; //修正滚动条位置
+}
+li {
+  padding: 5px;
+  .address {
+    font-size: 12px;
+    color: gray;
+  }
+}
+</style>
